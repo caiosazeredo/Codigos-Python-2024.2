00e6afd6f1387eb0679d5d00d131c14da10301f5
Caio Azeredo <caiosazeredo@cos.ufrj.br>
2024-10-02 19:56:27 -0300

    Exemplo 1
    Exemplo de calculo de imposto

---
+++ Aula3/exemplo1.py
@@ -0,0 +1,4 @@
+print(" Vamos calcular o imposto! ")
+valorproduto = float(input("Coloque o valor do produto:"))
+imposto = valorproduto * 0.1
+print(f"O valor que você terá que pagar de imposto desse produto é:{imposto}")
