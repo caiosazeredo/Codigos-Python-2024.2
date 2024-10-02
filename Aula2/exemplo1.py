1d8afb365627b5f5746c7f729eac57fee6f34be8
Caio Azeredo <caiosazeredo@cos.ufrj.br>
2024-10-02 19:32:04 -0300

    Exemplo 1 atualização
    Melhorei o mapa do tesouro

--- Aula2/exemplo1.py
+++ Aula2/exemplo1.py
@@ -1,5 +1,7 @@
+print("Este é o caminho para o mapa do tesouro")
 #Esses serão os passos do pirata
 print("Andar para direita")
 print("Andar para esquerda")
 #Esse aqui é um comentário 
 print("Andar para baixo")
+print("Procurar o X e cavar")
