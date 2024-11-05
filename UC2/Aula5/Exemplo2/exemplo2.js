d7428afe7b0756cfdd5f94b2c7c3e46256942964
Caio Azeredo <caiosazeredo@cos.ufrj.br>
2024-11-04 21:57:27 -0300

    Exercicios e exemplos aula 5

---
+++ UC2/Aula5/Exemplo2/exemplo2.js
@@ -0,0 +1,19 @@
+document.write("Ola terra")
+//alert('ola');
+console.log('ola, tudo bem?');
+var variavel1 = 'Ola mundo';
+//alert(variavel1);
+var variavel2 = 123;
+//alert(variavel2);
+console.log(variavel2);
+let variavel3 = 'Eu também sou uma variável';
+//alert(variavel3);
+
+/* Comentário
+em javascript
+em mútliplas linhas
+*/
+
+document.write("Ola marte"); 
+document.write('<h1> Eu sou um título</h1>');
+document.write('<title> titulo da pagina </title>')
